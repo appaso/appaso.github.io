f1e2e41ab7931ba21daa394abe2c92caaa51cdc6
Appaso Borate <appaso1686@gmail.com>
2022-12-23 14:44:44 +0530

    first commit

---
+++ __settings__.js
@@ -0,0 +1,23 @@
+ASSET_PREFIX = "";
+SCRIPT_PREFIX = "";
+SCENE_PATH = "1623195.json";
+CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': true,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+SCRIPTS = [ 116213642, 116213650, 116213652, 116213666, 116213669, 116213676 ];
+CONFIG_FILENAME = "config.json";
+INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: true,
+    useTouch: true
+};
+pc.script.legacy = false;
+PRELOAD_MODULES = [
+    {'moduleName' : 'BASIS', 'glueUrl' : 'files/assets/116213639/1/basis.wasm.js', 'wasmUrl' : 'files/assets/116213640/1/basis.wasm.wasm', 'fallbackUrl' : 'files/assets/116213638/1/basis.js', 'preload' : false},
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/116213635/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/116213636/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/116213634/1/ammo.js', 'preload' : true},
+];
